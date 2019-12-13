5bf1cfdc5baf8c80362a4ea48dd02b7196ef7cce
Pplociennik <przemx.84@gmail.com>
2019-12-13 01:00:52 +0100

    Release 0.9.0_1

--- src/main/java/com/goaleaf/repositories/UserRepository.java
+++ src/main/java/com/goaleaf/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.goaleaf.entities.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
