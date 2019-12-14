088b089273c7260c19023e1a899dca94a4f02022
Pplociennik <przemx.84@gmail.com>
2019-12-14 02:23:51 +0100

    Release 0.9.0_5

--- src/main/java/com/goaleaf/validators/FileConverter.java
+++ src/main/java/com/goaleaf/validators/FileConverter.java
@@ -2,10 +2,6 @@ package com.goaleaf.validators;
 
 import org.apache.commons.io.FileUtils;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
@@ -13,11 +9,9 @@ import java.util.Base64;
 public class FileConverter {
 
     public static String encodeFileToBase64Binary(File file) {
-
-        File input = scale(file);
         byte[] fileContent = new byte[0];
         try {
-            fileContent = FileUtils.readFileToByteArray(input);
+            fileContent = FileUtils.readFileToByteArray(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,7 +22,7 @@ public class FileConverter {
 
     public static File decodeFileFromBase64Binary(String string) {
         byte[] decodedBytes = Base64.getDecoder().decode(string);
-        File output = new File("image.jpg");
+        File output = new File("image.png");
         try {
             FileUtils.writeByteArrayToFile(output, decodedBytes);
         } catch (IOException e) {
@@ -36,35 +30,5 @@ public class FileConverter {
         }
 
         return output;
-    }
-
-    private static File scale(File input) {
-
-        final int w = 40;
-        final int h = 40;
-
-        Image image = null;
-        try {
-            image = ImageIO.read(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Image newImage = image.getScaledInstance(w, h, Image.SCALE_DEFAULT);
-
-        BufferedImage resultImage = null;
-        File resultFile = new File(".temp/RESULT_IMAGE.jpg");
-        try {
-
-            resultImage = ImageIO.read((ImageInputStream) newImage);
-
-            ImageIO.write(resultImage, "jpg", resultFile);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return resultFile;
-
     }
 }
